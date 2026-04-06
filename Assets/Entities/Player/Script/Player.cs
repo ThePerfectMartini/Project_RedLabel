bdb1393b665cb9f13d10c3fdcc8ffae0c866ca08
KilonXIII <linkzero1515@gmail.com>
2026-04-07 02:48:31 +0900

    UI 구현(임시)
    메인 메뉴 스타트 게임 종료 hp바 구현. 설정과 일시정지는 아직. 디자인도 구림 반드시 메인메뉴씬에서 시작할것.

--- Assets/Entities/Player/Script/Player.cs
+++ Assets/Entities/Player/Script/Player.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(PlayerMovementController))]
 public class Player : Entity
@@ -15,8 +16,11 @@ public class Player : Entity
     public float moveSpeed = 5f;
     public float runSpeed = 10f;
 
-// 수정?
-    protected override void Awake()
+	[Header("UI")]
+	public Image hpFillImage;//체력바 이미지 변수 추가
+
+	// 수정?
+	protected override void Awake()
     {
         base.Awake();
         StateMachine = new PlayerStateMachine(this);
@@ -30,7 +34,9 @@ public class Player : Entity
     {
         base.Start();
         StateMachine.Initialize(StateMachine.IdleState);
-    }
+
+		UpdateHPBar();
+	}
 
     private void OnEnable()
     {
@@ -60,15 +66,28 @@ public class Player : Entity
         
         StateMachine.ChangeState(StateMachine.HitState);
 
-        Debug.Log($"[{gameObject.name}] 피격! 받은 데미지: {hitData.damage} / 남은 체력: {currentHealth}");
+		UpdateHPBar();
+
+		Debug.Log($"[{gameObject.name}] 피격! 받은 데미지: {hitData.damage} / 남은 체력: {currentHealth}");
     }
 
-    // 사망 시점도 확인하고 싶다면 Die 메서드 역시 오버라이드할 수 있습니다.
-    protected override void Die()
+	// 사망 시점도 확인하고 싶다면 Die 메서드 역시 오버라이드할 수 있습니다.
+	protected override void Die()
     {
         Debug.Log($"[{gameObject.name}] 체력이 모두 소진되어 사망했습니다!");
         
         // 부모의 Die 로직(Destroy)을 실행하여 오브젝트를 파괴합니다.
         base.Die(); 
     }
+
+
+	// 체력바 UI를 업데이트하는 전용 함수 추가
+	private void UpdateHPBar()
+	{
+		if (hpFillImage != null)
+		{
+
+			hpFillImage.fillAmount = currentHealth / maxHealth;
+		}
+	}
 }
